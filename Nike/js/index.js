834cac2033d6c7621e3066a5793642ad16e0e939
xVFoxyVx <AnimeCross2006@gmail.com>
2026-02-18 22:46:02 +0300

    Add files via upload

---
+++ Nike/js/index.js
@@ -0,0 +1,31 @@
+import HeaderFixed from "./header.js";
+import BurgerMenu from "./burger.js";
+import { productSlider } from "./product-slider.js";
+
+try {
+    const headerFixed = new HeaderFixed({
+        HEADER: "header",
+        HEADER_FIXED: "header--fixed",
+    });
+
+    new BurgerMenu(
+        {
+            BURGER: "burger",
+            BURGER_OPEN: "burger--open",
+            HEADER_MENU: "header__menu",
+            HEADER_MENU_OPEN: "header__menu--open",
+            lABEL: {
+                OPEN: "Открыть меню",
+                CLOSE: "Закрыть меню",
+            },
+            PAGE_BODY: "page__body",
+            PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+            MENU_LINK: "menu__link",
+            BREAKPOINT: 768,
+            MAIN: "main",
+        }
+    );
+    productSlider ();
+} catch (error) {
+    console.error(error);
+}
