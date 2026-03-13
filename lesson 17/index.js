8c62b0d04c0fcd27441e6f0b17ded36685ad95bf
xVFoxyVx <AnimeCross2006@gmail.com>
2026-03-13 20:55:19 +0300

    Add files via upload

---
+++ lesson 17/index.js
@@ -0,0 +1,19 @@
+const firstName = "Timur";
+const lastName = "Tankovich";
+const isStudent = true;
+
+const age = 20; 
+const currentYear = 2026;
+const birthYear = (currentYear - age);
+
+const textInfo = `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик/ученица курса: ${isStudent}`;
+console.log(textInfo);
+
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+
+//result 123456789falsetrue
